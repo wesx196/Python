7b0ca4d434a7dded1a4979166149ad2c7185fb5f
Wei Lu <wlu@thelabconsulting.com>
2025-01-07 13:33:46 -0600

    gui

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 
-print(data["temp"])
+#print(data["temp"])
 
 temp = data["temp"]
 
@@ -16,5 +16,13 @@ monday = data[data.day == 'Monday']
 monday_temp = monday.temp[0]
 monday_temp_2 = monday.temp
 
-print(monday_temp)
-print(monday_temp_2)
+#print(monday_temp)
+#print(monday_temp_2)
+
+# numbers = [1,2,3]
+# new_numbers = [n + 1 for n in numbers]
+# print(new_numbers)
+
+number_list = range(1,5)
+double_list = [n * 2 for n in number_list]
+print(double_list)
